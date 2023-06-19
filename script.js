9eaf5f61c93ae90e8229bec09e1491d04805738f
陳俊佑 <35156817+junyou1998@users.noreply.github.com>
2023-06-19 13:47:17 +0800

    match multiple language

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 const { ref, reactive, onMounted } = Vue;
 const App = {
     setup() {
-        const idx = ref(0);
         let pokeDict = {};
-
-        const poke = reactive({ name: "",img:"" });
-
+        const poke = reactive({ name_en: "",name_cn:"",img:"" });
         const getRand = ()=>Math.floor(Math.random() * 100) + 1
-        
-        const basePokeUrl = "https://pokeapi.co/api/v2/pokemon/"
+        const basePokeApi = "https://pokeapi.co/api/v2/pokemon/"
         const pokeDictApi = "https://raw.githubusercontent.com/junyou1998/Pokemon/main/api/PokeApi.json"
+
         onMounted(() => {
             axios.get(pokeDictApi).then((res) => {
                 pokeDict = res.data;
-                console.log(pokeDict)
-
-                axios.get(basePokeUrl+getRand()).then((res) => {
-                    console.log(res.data)
-
-                    poke.img = res.data.sprites.other.dream_world.front_default
+                axios.get(basePokeApi+getRand()).then((res2) => {
+                    pokeInfo = res2.data
+                    poke.img = pokeInfo.sprites.other.dream_world.front_default
+                    poke.name_en = pokeInfo.name
+                    const matchPoke = pokeDict.find(pokemon => pokemon.name.en.toLowerCase() == pokeInfo.name);
+                    poke.name_cn = matchPoke.name.zh
+                    
                 });
             });
         });
 
-        return { idx, poke };
+        return { poke };
     },
 };
 
 Vue.createApp(App).mount("#app");
+
