a161bd8e7b1c4e592842537ea6a4e3e7f1048710
陳俊佑 <35156817+junyou1998@users.noreply.github.com>
2023-06-19 12:00:44 +0800

    get two api

--- script.js
+++ script.js
@@ -2,17 +2,29 @@ const { ref, reactive, onMounted } = Vue;
 const App = {
     setup() {
         const idx = ref(0);
+        let pokeDict = {};
 
-        const poke = reactive({ name: "" });
-        const theFn = () => {};
+        const poke = reactive({ name: "",img:"" });
 
+        const getRand = ()=>Math.floor(Math.random() * 100) + 1
+        
+        const basePokeUrl = "https://pokeapi.co/api/v2/pokemon/"
+        const pokeDictApi = "https://raw.githubusercontent.com/junyou1998/Pokemon/main/api/PokeApi.json"
         onMounted(() => {
-            axios.get("/").then((res) => {
-                console.log(res.data);
+            axios.get(pokeDictApi).then((res) => {
+                pokeDict = res.data;
+                console.log(pokeDict)
+
+                axios.get(basePokeUrl+getRand()).then((res) => {
+                    console.log(res.data)
+
+                    poke.img = res.data.sprites.other.dream_world.front_default
+                });
             });
         });
 
-        return { idx, poke, theFn };
+        return { idx, poke };
     },
 };
+
 Vue.createApp(App).mount("#app");
