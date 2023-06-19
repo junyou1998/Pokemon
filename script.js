440794ec1622fba623a8fa3d54d4a10a8c9a5085
陳俊佑 <35156817+junyou1998@users.noreply.github.com>
2023-06-19 13:57:27 +0800

    add hp att def

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const { ref, reactive, onMounted } = Vue;
 const App = {
     setup() {
         let pokeDict = {};
-        const poke = reactive({ name_en: "",name_cn:"",img:"" });
+        const poke = reactive({ name_en: "",name_cn:"",img:"",hp:0,att:0,def:0 });
         const getRand = ()=>Math.floor(Math.random() * 100) + 1
         const basePokeApi = "https://pokeapi.co/api/v2/pokemon/"
         const pokeDictApi = "https://raw.githubusercontent.com/junyou1998/Pokemon/main/api/PokeApi.json"
@@ -16,6 +16,10 @@ const App = {
                     poke.name_en = pokeInfo.name
                     const matchPoke = pokeDict.find(pokemon => pokemon.name.en.toLowerCase() == pokeInfo.name);
                     poke.name_cn = matchPoke.name.zh
+
+                    poke.hp = pokeInfo.stats[0].base_stat
+                    poke.att = pokeInfo.stats[1].base_stat
+                    poke.def = pokeInfo.stats[2].base_stat
                     
                 });
             });
