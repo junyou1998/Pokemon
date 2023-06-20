5346f81606d065ef9981586e84437f8f4cd56377
陳俊佑 <35156817+junyou1998@users.noreply.github.com>
2023-06-20 09:54:51 +0800

    only call once api1

--- script.js
+++ script.js
@@ -2,35 +2,43 @@ const { ref, reactive, onMounted } = Vue;
 const App = {
     setup() {
         let pokeDict = {};
-        const poke = reactive({ name_en: "",name_cn:"",img:"",hp:0,att:0,def:0 });
-        const getRand = ()=>Math.floor(Math.random() * 100) + 1
-        const basePokeApi = "https://pokeapi.co/api/v2/pokemon/"
-        const pokeDictApi = "https://raw.githubusercontent.com/junyou1998/Pokemon/main/api/PokeApi.json"
+        const poke = reactive({ name_en: "", name_cn: "", img: "", hp: 0, att: 0, def: 0 });
+        const getRand = () => Math.floor(Math.random() * 100) + 1;
+        const basePokeApi = "https://pokeapi.co/api/v2/pokemon/";
+        const pokeDictApi = "https://raw.githubusercontent.com/junyou1998/Pokemon/main/api/PokeApi.json";
 
-        const genPoke = ()=>{
-            axios.get(pokeDictApi).then((res) => {
+        const getPokeDict = async () => {
+            try {
+                const res = await axios.get(pokeDictApi);
                 pokeDict = res.data;
-                axios.get(basePokeApi+getRand()).then((res2) => {
-                    pokeInfo = res2.data
-                    poke.img = pokeInfo.sprites.other.dream_world.front_default
-                    poke.name_en = pokeInfo.name
-                    const matchPoke = pokeDict.find(pokemon => pokemon.name.en.toLowerCase() == pokeInfo.name);
-                    poke.name_cn = matchPoke.name.zh
+            } catch (error) {
+                console.log("get error");
+            }
+        };
 
-                    poke.hp = pokeInfo.stats[0].base_stat
-                    poke.att = pokeInfo.stats[1].base_stat
-                    poke.def = pokeInfo.stats[2].base_stat
-                    
-                });
-            });
-        }
-        onMounted(() => {
-            genPoke()
+        const getPokeInfo = async () => {
+            try {
+                const res = await axios.get(basePokeApi + getRand());
+                pokeInfo = res.data;
+                poke.img = pokeInfo.sprites.other.dream_world.front_default;
+                poke.name_en = pokeInfo.name;
+                const matchPoke = pokeDict.find((pokemon) => pokemon.name.en.toLowerCase() == pokeInfo.name);
+                poke.name_cn = matchPoke.name.zh;
+                poke.hp = pokeInfo.stats[0].base_stat;
+                poke.att = pokeInfo.stats[1].base_stat;
+                poke.def = pokeInfo.stats[2].base_stat;
+            } catch (error) {
+                console.log("get error");
+            }
+        };
+        
+        onMounted(async () => {
+            await getPokeDict();
+            await getPokeInfo();
         });
 
-        return { poke, genPoke };
+        return { poke, getPokeInfo };
     },
 };
 
 Vue.createApp(App).mount("#app");
-
